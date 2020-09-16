a11b2a5c458fd63367d1cae3922b6b6949d1d58f
samanyu6 <samanyu6@yahoo.co.in>
2020-09-16 10:31:49 +0530

    working server with dummy vars

---
+++ src/test/java/com/samanyu/security/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samanyu.security.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
